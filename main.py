b9bb165c689b7fe80c1442052d1c85ac59dac496
Tyler Pearson <tyler.a.pear@gmail.com>
2025-06-28 19:52:22 -0500

    add game clock and save dt after each cycle

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         screen.fill((0,0,0))
@@ -12,10 +14,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-
-
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
