d03483769645c4c64e4ea9ff1f7bbafdcf08b12c
Tyler Pearson <tyler.a.pear@gmail.com>
2025-06-28 23:03:55 -0500

    fixes asteroids not drawing radius

--- asteroid.py
+++ asteroid.py
@@ -3,8 +3,11 @@ from circleshape import CircleShape
 
 class Asteroid(CircleShape):
 
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
     def draw(self, screen):
-        pygame.draw.circle(screen, (255,255,255), self.position, 2)
+        pygame.draw.circle(screen, (255,255,255), self.position, self.radius, 2)
 
     def update(self, dt):
         self.position += self.velocity * dt
